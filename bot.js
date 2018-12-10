7ba8f41ab73b9e31edcc4348eaa3bd7d7b8efa69
dadbass <45670534+dadbass@users.noreply.github.com>
2018-12-10 11:38:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const client = new Discord.Client();
 const prefix = "#";
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`help`,"http://twitch.tv/S-F")
+client.user.setGame(`#help | By: éMyyy `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
